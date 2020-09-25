066a735b811c4f4dafb279e1f972e8dcdcc06897
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 22:20:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :name, :grade
-  def initialize(id=nil, name, grade)
+  def initialize(name, grade, id = nil)
+    @id = id
     @name = name
     @grade = grade
   end
