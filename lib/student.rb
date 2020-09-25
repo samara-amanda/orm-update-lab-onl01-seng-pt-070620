3e92ed00ff7032396db66a35de6627707fc0e629
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 22:36:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,10 +46,13 @@ class Student
     student.save
     student
   end
-  
-  def self.new_from_db
-  end
 
+  def self.new_from_db(row)
+  id = row[0]
+  name = row[1]
+  grade = row[2]
+  self.new(id, name, grade)
+end 
 
 
 end
