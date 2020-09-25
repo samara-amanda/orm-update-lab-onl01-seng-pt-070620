615577709fe9932a08f2b5ecd0bfb89b1bb50edb
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 22:36:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,5 +54,14 @@ class Student
     self.new(id, name, grade)
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
