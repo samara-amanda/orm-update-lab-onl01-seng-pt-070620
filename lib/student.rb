42806061202ef7b833bed07be6090ee7bfeef3e9
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 22:23:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,11 +6,24 @@ class Student
   #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
+
   def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXITSTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
 
 
 end
